06c771d99dad613d921c197be57bc1b081839200
stabmeldys <lukas.les93@gmail.com>
2024-08-25 17:48:39 +0300

    client as struct

--- open-client/src/lib.rs
+++ open-client/src/lib.rs
@@ -1,42 +1,55 @@
 use std::io::{self, BufRead, BufReader, Write};
 use std::net::TcpStream;
 
-fn send_command(command: &str) -> io::Result<String> {
-    // Connect to the server
-    let mut stream = TcpStream::connect("127.0.0.1:42069")?;
-    println!("Connected to the server.");
-
-    // Send the command to the server
-    stream.write_all(command.as_bytes())?;
-    stream.write_all(b"\n")?; // Send a newline to indicate the end of the command
-
-    // Read the response from the server
-    let mut reader = BufReader::new(&stream);
-    let mut response = String::new();
-    reader.read_line(&mut response)?;
-
-    Ok(response)
+struct Client {
+    host: String,
 }
 
-pub fn insert(path: &str, value: &str) -> io::Result<String> {
-    let command = format!("insert {} {}", path, value);
-    send_command(&command)
+impl Client {
+    fn new(host: &str, port: usize) -> Self {
+        Client {
+            host: format!("{}:{}", host, port),
+        }
+    }
+
+    fn send_command(&self, command: &str) -> io::Result<String> {
+        // Connect to the server
+        let mut stream = TcpStream::connect(&self.host)?;
+        println!("Connected to the server.");
+    
+        // Send the command to the server
+        stream.write_all(command.as_bytes())?;
+        stream.write_all(b"\n")?; // Send a newline to indicate the end of the command
+    
+        // Read the response from the server
+        let mut reader = BufReader::new(&stream);
+        let mut response = String::new();
+        reader.read_line(&mut response)?;
+    
+        Ok(response)
+    }
+
+    pub fn insert(&self, path: &str, value: &str) -> io::Result<String> {
+        let command = format!("insert {} {}", path, value);
+        self.send_command(&command)
+    }
+
+    pub fn get(&self, path: &str) -> io::Result<String> {
+        let command = format!("get {}", path);
+        self.send_command(&command)
+    }
+    
+    pub fn hit(&self, path: &str) -> io::Result<String> {
+        let command = format!("hit {}", path);
+        self.send_command(&command)
+    }
+    
+    pub fn delete(&self, path: &str) -> io::Result<String> {
+        let command = format!("delete {}", path);
+        self.send_command(&command)
+    }
 }
 
-pub fn get(path: &str) -> io::Result<String> {
-    let command = format!("get {}", path);
-    send_command(&command)
-}
-
-pub fn hit(path: &str) -> io::Result<String> {
-    let command = format!("hit {}", path);
-    send_command(&command)
-}
-
-pub fn delete(path: &str) -> io::Result<String> {
-    let command = format!("delete {}", path);
-    send_command(&command)
-}
 
 #[cfg(test)]
 mod tests {
@@ -44,7 +57,8 @@ mod tests {
 
     #[test]
     fn test_client() {
-        let result = insert("wau", "audi");
-        dbg!(result);
+        let client = Client::new("127.0.0.1", 42069);
+        let result = client.insert("wau", "audi");
+        dbg!(result.unwrap());
     }
 }
