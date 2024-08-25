0b8184f16e2409caddf86afb6be021b78aa1a322
stabmeldys <lukas.les93@gmail.com>
2024-08-25 06:18:24 +0300

    remove target dir

--- open-server/target/debug/deps/open_server-0c962a584471f909.d
+++
@@ -1,5 +0,0 @@
-/mnt/e/github/girios/open-server/target/debug/deps/libopen_server-0c962a584471f909.rmeta: src/main.rs
-
-/mnt/e/github/girios/open-server/target/debug/deps/open_server-0c962a584471f909.d: src/main.rs
-
-src/main.rs:
--- open-server/target/debug/deps/open_server-5e6007a21f5aac77.d
+++
@@ -1,5 +0,0 @@
-/mnt/e/github/girios/open-server/target/debug/deps/libopen_server-5e6007a21f5aac77.rmeta: src/main.rs
-
-/mnt/e/github/girios/open-server/target/debug/deps/open_server-5e6007a21f5aac77.d: src/main.rs
-
-src/main.rs:
