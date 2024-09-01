290081ad131808a762cee0008248ac900e399356
stabmeldys <lukas.les93@gmail.com>
2024-09-01 07:24:12 +0300

    added new line in responses

--- common/src/server/response.rs
+++ common/src/server/response.rs
@@ -11,9 +11,9 @@ pub enum ResponseStatus {
 impl fmt::Display for ResponseStatus {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ResponseStatus::Ok(ref s) => writeln!(f, "ok:{}", s),
-            ResponseStatus::NoData(ref s) => writeln!(f, "nodata:{}", s),
-            ResponseStatus::Error(ref e) => writeln!(f, "e: {}", e),
+            ResponseStatus::Ok(ref s) => writeln!(f, "ok:\n{}", s),
+            ResponseStatus::NoData(ref s) => writeln!(f, "nodata:\n{}", s),
+            ResponseStatus::Error(ref e) => writeln!(f, "e:\n{}", e),
         }
     }
 }
